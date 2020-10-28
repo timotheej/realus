334463481f1f8c0a7f93db8916d31ab630588318
Timothée Jezek <timothee.jezek@gmail.com>
2020-10-28 14:02:05 +0100

    header content

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
 <div>
-    <header>
+    <header class="header">
         <Nav />
+        <div class="header__contain container">
+            <div class="header__content">
+                <h1 class="font-light text-white leading-none">Découvre qui tu es <span class="font-medium">réellement</span></h1>
+                <span class="text-white text-lg font-light mt-4 block">Compare toi, améliore tes relations</span>
+
+                <a href="#" class="btn btn--large btn--darky mt-12 inline-block">Commence maintenant</a>
+            </div>
+        </div>
     </header>
     <section class="main">
         <div class="container">
@@ -18,11 +26,20 @@ export default {}
 <style lang="scss">
 @import "~/assets/css/main.scss";
 
-header {
-    @apply h-screen relative;
+.header {
+    @apply h-screen relative flex flex-col;
     background-image: url('~assets/images/header-background.jpg');
     background-size: cover;
     z-index: 0;
+
+    &__contain {
+        @apply mx-auto flex items-center;
+        height: calc(100% - 12rem);
+    }
+
+    &__content {
+        @apply max-w-xl;
+    }
 
     &::before {
         content: "";
